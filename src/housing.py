59038f56035b423e09b3dc444b54673d72f731a9
Cody Brown <cody.thecodeman71@gmail.com>
2026-05-13 14:19:10 -0700

    finished program before input validation

--- src/housing.py
+++ src/housing.py
@@ -23,26 +23,31 @@ for districts in range (2):
     new_evacuees_expected = 0
     occupancy_rate = 0
     average_occupancy_rate_for_district = 0
+    calc_occupancy_rate = 0
 
     # The start of our loop for our shelters.
     for shelters in range(3):
-        user_total_capacity = input("Enter Total Capacity: ")
+        user_total_capacity = int(input("Enter Total Capacity: "))
+            while user_total_capacity > 0
         total_capacity += user_total_capacity
-
-        user_people_sheltered = input("Enter people sheltered: ")
+        
+        user_people_sheltered = int(input("Enter people sheltered: "))
         people_sheltered += user_people_sheltered
 
-        user_new_evacuees_expected = input("Enter new evacuees expected: ")
+        user_new_evacuees_expected = int(input("Enter new evacuees expected: "))
         new_evacuees_expected += user_new_evacuees_expected
 
-        occupancy_rate = people_sheltered / total_capacity * PERCENTAGE_RATE
+
+        calc_occupancy_rate = people_sheltered / total_capacity * PERCENTAGE_RATE
+        occupancy_rate += calc_occupancy_rate
     
     print("*** District Summary ***")
     print("-------------------")
-    print("Average occupancy rate: ", (occupancy_rate / 3))
-    print("Total sheltered: ", people_sheltered
-Total new evacuees expected: 80
-Projected occupancy rate: 101.82% 
+    print("Average occupancy rate: %", (occupancy_rate / 3))
+    print("Total Capacity: ", total_capacity)
+    print("Total sheltered: ", people_sheltered)
+    print("Total new evacuees expected: ", new_evacuees_expected)
+    print("Projected occupancy rate: %",(people_sheltered + new_evacuees_expected) / total_capacity * PERCENTAGE_RATE)
 
 
 print("Data collection complete for all districts.")
