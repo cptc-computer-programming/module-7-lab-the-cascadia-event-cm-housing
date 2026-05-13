3241c36b78fbc169e6cf422362f0d00fdd009138
Cody Brown <cody.thecodeman71@gmail.com>
2026-05-13 13:32:46 -0700

    Added our intiated Variables

--- src/housing.py
+++ src/housing.py
@@ -9,6 +9,22 @@ MIN_PEOPLE_SHELTERED = 1
 MIN_NEW_EVACUEES = 0
 
 
+# Cloverpark intiated variables.
+clover_total_capacity = 0
+clover_people_sheltered = 0
+clover_new_evacuees_expected = 0
+clover_occupancy_rate = 0
+
+# Steilacoom intiated variables.
+steilacoom_total_capacity = 0
+steilacoom_people_sheltered = 0
+steilacoom_new_evacuees_expected = 0
+steilacoom_occupancy_rate = 0
+
+
+
+
+
 # TODO: Process shelter occupancy data for both districts.
 
 # TODO: For each district, process all shelters.
